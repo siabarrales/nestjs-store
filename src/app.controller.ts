29624287be80bb40542e46eae1dcc33d6aba3dbb
Sebastián Avendaño Barrales <siabarrales@gmail.com>
2023-04-21 20:52:18 -0400

    Params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('new-endpoint')
+  newEndpoint() {
+    return 'This is a new endpoint!';
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    return `Product ${id}`;
   }
 }
