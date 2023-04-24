4bb1a7f207e02fa5727d40b80ea5c7952d27eb6e
Sebastián Avendaño Barrales <siabarrales@gmail.com>
2023-04-24 12:55:39 -0400

    put, delete, http codes

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,19 +1,63 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Body,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
   getProducts(@Query('limit') limit = 100, @Query('offset') offset = 0) {
-    return `Products: limit => ${limit}, offset => ${offset}`;
+    return {
+      message: 'Products List',
+      limit,
+      offset,
+    };
   }
   //Para que las dos rutas no choquen, siempre deben ir primero las que no son dinamicas
   @Get('filter')
   getProductFilter() {
-    return `Product Filter`;
+    return {
+      message: 'You are in the filter',
+    };
   }
 
+  //Enviar un HttpCode en particular. En este caso, el 202
   @Get(':id')
+  @HttpCode(HttpStatus.ACCEPTED)
   getProduct(@Param('id') id: string) {
-    return `Product ${id}`;
+    return {
+      message: `Product ${id}`,
+    };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'Action to create a product',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() payload: any) {
+    return {
+      message: `Action to update a product ${id}`,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return {
+      message: `Action to delete a product ${id}`,
+    };
   }
 }
