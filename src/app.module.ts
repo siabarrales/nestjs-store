9b029edf7f691f74715815dea67f5697f4ed6e6e
Sebastián Avendaño Barrales <siabarrales@gmail.com>
2023-04-24 23:04:19 -0400

    removing categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
+  controllers: [AppController, ProductsController],
   providers: [AppService, ProductsService],
 })
 export class AppModule {}
