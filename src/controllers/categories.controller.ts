372fa3797ee2549111ac9a241e84b1e37034a866
Sebastián Avendaño Barrales <siabarrales@gmail.com>
2023-04-24 23:03:21 -0400

    removing categories, adding whitelist to validation pipe

--- src/controllers/categories.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Param } from '@nestjs/common';
-
-@Controller('categories')
-export class CategoriesController {
-  @Get(':id/products/:productId')
-  getProduct(@Param('id') id: string, @Param('productId') productId: string) {
-    return `Category ${id}, Product ${productId}`;
-  }
-}
